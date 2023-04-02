bf338f63d555092b20fe0c138d89b0cda951db8b
songen <614750803@qq.com>
2023-04-02 22:41:25 +0800

    添加 menu

--- client.go
+++ client.go
@@ -11,12 +11,14 @@ type Client struct {
 	ClientPort int
 	Name       string
 	Conn       net.Conn
+	flag       int
 }
 
 func NewClient(serverIP string, serverPort int) *Client {
 	client := &Client{
 		ClientIP:   serverIP,
 		ClientPort: serverPort,
+		flag:       77,
 	}
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIP, serverPort))
 	if err != nil {
@@ -35,7 +37,44 @@ func init() {
 	flag.StringVar(&serverIP, "ip", "127.0.0.1", "设置链接ip地址")
 	flag.IntVar(&serverPort, "port", 8090, "设置链接port")
 }
+func (c *Client) Run() {
+	for c.flag != 0 {
+		for c.Menu() != true {
+		}
+		switch c.flag {
+		case 1:
+			fmt.Println("公聊模式")
+			break
 
+		case 2:
+			fmt.Println("私聊模式")
+			break
+
+		case 3:
+			fmt.Println("更名")
+			break
+
+		}
+	}
+	fmt.Println("关闭链接")
+}
+func (c *Client) Menu() bool {
+	var flag int
+	fmt.Println("1.公聊模式...")
+	fmt.Println("2.私聊模式...")
+	fmt.Println("3.更名模式...")
+	fmt.Println("0.退出...")
+
+	fmt.Scanln(&flag)
+
+	if flag >= 0 && flag <= 3 {
+		c.flag = flag
+		return true
+	} else {
+		fmt.Println(">>>>>>请输入合法数字<<<<<<")
+		return false
+	}
+}
 func main() {
 	flag.Parse()
 	client := NewClient(serverIP, serverPort)
@@ -44,6 +83,5 @@ func main() {
 		return
 	}
 	fmt.Println("=======>链接成功<=======")
-
-	select {}
+	client.Run()
 }
